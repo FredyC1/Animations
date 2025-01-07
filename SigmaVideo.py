25bcbf3b2f3c7a801300e5092efa30c38f88f61f
Fredy <alexcabreraap@icloud.com>
2025-01-07 13:47:24 -0500

    Pushing first part of intro

--- SigmaVideo.py
+++ SigmaVideo.py
@@ -36,17 +36,50 @@ class Intro(Scene):
 
         #image 
         functionimg = ImageMobject("images/function.jpeg")
-        # functionimg.scale()
         self.play(FadeIn(functionimg))
-
+        #revrese
         self.play(FadeOut(functionimg), run_time=2)
         self.play(Unwrite(functionTextEquals))
         self.play(FadeIn(arrow, arrow2, input, output, box, box2), run_time=1)
         self.play(functionText.animate.shift(RIGHT*5+DOWN*3), run_time=2)
         self.play(Unwrite(functionText))
-        
+        #Sigma/sigmoid functions
         sigmaText = MathTex(r"\sum_{i=1}^n f(i)", color=BLUE)
         sigmaText[0][2].set_color(YELLOW)  
         sigmaText[0][7].set_color(YELLOW)
 
         self.play(Write(sigmaText))
+        self.play(Unwrite(sigmaText))
+        sigmoidText = MathTex(r"\sigma","(","x",")",color=BLUE)
+        sigmoidText[2].set_color(YELLOW)
+        self.play(Write(sigmoidText))
+        self.play(Unwrite(sigmoidText))
+
+        # All functions
+        limit_tex = MathTex(r"\lim_{x \to a} f(x)", color=BLUE).set_height(0.5)
+        limit_tex[0][3].set_color(YELLOW)  
+        limit_tex[0][8].set_color(YELLOW)
+        derivative_tex = MathTex(r"\frac{d}{d","x","}","x","^2 = 2","x", color=BLUE).set_height(0.6)
+        derivative_tex[1].set_color(YELLOW)
+        derivative_tex[3].set_color(YELLOW)
+        derivative_tex[5].set_color(YELLOW)
+        integral_tex = MathTex(r"\int_{a}^{b} f(","x",") \, d","x", color=BLUE).set_height(0.7)
+        integral_tex[1].set_color(YELLOW)
+        integral_tex[3].set_color(YELLOW)
+        gradient_tex = MathTex(r"\nabla f(","x", ", y, z)", color=BLUE).set_height(0.3)
+        gradient_tex[1].set_color(YELLOW)
+        divergence_tex = MathTex(r"\nabla \cdot \mathbf{F}", color=BLUE).set_height(0.4)
+
+        self.play(Write(limit_tex), run_time=0.5)
+        self.play(Unwrite(limit_tex), run_time=0.5)
+        self.play(Write(derivative_tex), run_time=0.5)
+        self.play(Unwrite(derivative_tex), run_time=0.5)
+        self.play(Write(integral_tex), run_time=0.5)
+        self.play(Unwrite(integral_tex), run_time=0.5)
+        self.play(Write(gradient_tex), run_time=0.5)
+        self.play(Unwrite(gradient_tex), run_time=0.5)
+        self.play(Write(divergence_tex), run_time=0.5)
+        self.play(Unwrite(divergence_tex), run_time=0.5)
+        
+
+
